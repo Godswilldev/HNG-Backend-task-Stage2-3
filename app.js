6807f95a9437c47d7bff5203a4e6b22cae3aab5c
Godswill Edet <gimmex1@gmail.com>
2021-08-20 19:45:26 +0100

    final commit

--- app.js
+++ app.js
@@ -20,13 +20,12 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => res.render("contact"));
 
 app.post("/send", (req, res) => {
-  const { name, email, phone, company, message } = req.body;
+  const { name, email, phone, message } = req.body;
   const output = `
     <h3>You have a new message</h3>
         <h4>Contact Details</h4>
     <ul>
         <li>Name: ${name} </li>
-        <li>Company: ${company} </li>
         <li>Email: ${email} </li>
         <li>Phone: ${phone} </li>    
     </ul>
@@ -56,15 +55,9 @@ app.post("/send", (req, res) => {
       from: '"Nodemailer message" <smtp.mailtrap.io>', // sender address
       to: "gimmex1@gmail.com, godswill@godswill.tech, smtp.mailtrap.io", // list of receivers
       subject: "Node Message Request", // Subject line
-      text: "Hello world?", // plain text body
+      text: output, // plain text body
       html: output, // html body
     });
-
-    // console.log("Message sent: %s", info.messageId);
-    // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
     res.render("contact", { msg: "Your message has been Sent" });
   };
